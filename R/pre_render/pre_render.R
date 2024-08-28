a824cf2850a0f61e7a5cca18f1c5f35db75290b4
Tylar <murray.tylar@gmail.com>
2024-08-28 14:37:37 -0400

    setup for ctd_reports. too big for quarto to handle

--- R/pre_render/pre_render.R
+++ R/pre_render/pre_render.R
@@ -1,4 +1,4 @@
 # TODO: download data
 # TODO: data preprocessing
 source("R/pre_render/generate_cruise_reports.R")
-#source("R/pre_render/generate_ctd_reports.R")
+source("R/pre_render/generate_ctd_reports.R")
