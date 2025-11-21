0384b3494b4e8f28eff8bde022b4c2312926b82a
Tylar <murray.tylar@gmail.com>
2025-11-21 15:08:34 -0500

    rm erddap setup (its in another file

--- R/pre_render/download_cruises.R
+++ R/pre_render/download_cruises.R
@@ -1,5 +1,4 @@
 library("purrr") # used to loop through lists
-library("rerddap") # used to access different ERDDAPs
 library("dplyr") # used to manipulate variables
 library("here") # used to get relative directory location to a project
 library("glue")
@@ -8,9 +7,6 @@ library("fs")
 source(here::here("cruise_list.R"))
 source(here::here("R/download_cruise.R"))
 
-# this database contains CTD data from Walton Smith Cruises
-database <- "https://gcoos5.geos.tamu.edu/erddap/"
-Sys.setenv(RERDDAP_DEFAULT_URL = database)
 
 # Set folder location
 loc <- here("data", "01_raw", "ctd")
