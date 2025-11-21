d7081b465cdd360c6e0c78bf04c987d8923f9a9b
Tylar <murray.tylar@gmail.com>
2025-11-21 16:18:22 -0500

    + ctd listing. update prerender.

---
+++ R/pre_render/generate_ctd_reports.R
@@ -0,0 +1,46 @@
+# creates a report template .qmd for each
+REPORT_TEMPLATE = "ctd_reports/ctd_reports_template.qmd"
+REPORTS_DIR = "ctd_reports/ctd_reports"
+
+if (!nzchar(system.file(package = "librarian"))) {
+  install.packages("librarian")
+}
+librarian::shelf(
+  glue,
+  whisker  
+)
+
+# Proceed if rendering the whole project, exit otherwise
+if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
+  quit()
+}
+
+# create the template
+templ <- readLines(REPORT_TEMPLATE) 
+templ <- gsub(
+  "WS16074_WS16074_WS16074stn2", "{{cast_id}}", templ
+)
+
+dir.create(REPORTS_DIR, showWarnings=FALSE)
+
+# === iterate through the data structure 
+# Set the root directory where the folders are located
+folder <- "data/01_raw/ctd"
+
+files <- list.files(folder, full.names = TRUE)
+
+# Loop through each file in the current folder (station)
+for (file in files) {
+  # Print the folder name and the filename
+  # print(paste("Folder:", folder, "File   :", basename(file)))
+  cast_id <- sub(".csv", "", basename(file))
+  params = list(
+    cast_id = cast_id
+  )
+  print(glue("=== creating template for '{cast_id}' ==="))
+  writeLines(
+    whisker.render(templ, params),
+    file.path(REPORTS_DIR, glue("{cast_id}.qmd"))
+  )
+}
+
--- R/pre_render/pre_render.R
+++ R/pre_render/pre_render.R
@@ -2,4 +2,5 @@ source("R/pre_render/download_cruises.R")
 
 # TODO: data preprocessing
 
-source("R/pre_render/generate_cruise_reports.R")
+#source("R/pre_render/generate_cruise_reports.R")
+source("R/pre_render/generate_ctd_reports.R")
