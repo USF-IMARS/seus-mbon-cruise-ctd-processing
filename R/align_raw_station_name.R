a473e96b40b90051142ce93d2cdbea8bfc45cd40
Tylar <murray.tylar@gmail.com>
2025-12-31 19:01:08 -0500

    implement pipeline for standardizing station names.

---
+++ R/align_raw_station_name.R
@@ -0,0 +1,6 @@
+align_raw_station_name <- function(rawStationName){
+  # TODO: map the rawStationName to a standard name from Station_Mean_Coords.csv
+
+  # For now, just return the input name (no changes)
+  return(rawStationName)
+}
