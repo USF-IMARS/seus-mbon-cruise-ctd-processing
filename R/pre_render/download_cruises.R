dd19daff5c7e506b7ecc5934023c5ef7d2db880a
Tylar <murray.tylar@gmail.com>
2026-01-20 16:45:27 -0500

    download cruises using ctd_datasetid_cruisename_stationname_mapping.csv

--- R/pre_render/download_cruises.R
+++ R/pre_render/download_cruises.R
@@ -1,16 +1,48 @@
-library("purrr") # used to loop through lists
 library("dplyr") # used to manipulate variables
 library("here") # used to get relative directory location to a project
 library("glue")
 library("fs")
+library("readr")
+library("rerddap") # used to access different ERDDAPs
 
-source(here::here("cruise_list.R"))
-source(here::here("R/download_cruise.R"))
+# this database contains CTD data 
+database <- "https://gcoos5.geos.tamu.edu/erddap/"
+Sys.setenv(RERDDAP_DEFAULT_URL = database)
 
+# Source the filename alignment function
+source(here::here("R/align_raw_ctd_filename.R"))
+
+# Read the mapping CSV to get dataset IDs
+mapping <- read_csv(here::here("data/ctd_datasetid_cruisename_stationname_mapping.csv"), 
+                    show_col_types = FALSE)
 
 # Set folder location
-loc <- here("data", "01_raw", "ctd")
+loc <- here("data", "01_raw")
+fs::dir_create(loc)
 
-for (i in seq(cruise_list)) {
-  try(download_cruise(cruise_list[i], loc), silent = TRUE)
+# Download each dataset from the mapping
+for (i in seq_len(nrow(mapping))) {
+  dataset_id <- mapping$dataset_id[i]
+  
+  # Get aligned filename
+  raw_filename <- paste0(dataset_id, ".csv")
+  aligned_filename <- align_raw_ctd_filename(raw_filename)
+  
+  # create file path with aligned filename (directly in 01_raw/ctd)
+  file_path <- here(loc, aligned_filename)
+  
+  # skip already downloaded files
+  if (fs::file_exists(file_path)) {
+    cat("Skipping:", aligned_filename, "\n")
+    next
+  }
+  
+  cat("Downloading:", dataset_id, "->", aligned_filename, "\n")
+  
+  # download data
+  out <- info(dataset_id)
+  ctd_data <- tabledap(out, url = eurl(), store = disk())
+  write.csv(ctd_data, file_path, row.names = FALSE)
 }
+
+cat("\nDownload complete!\n")
--- R/pre_render/pre_render.R
+++ R/pre_render/pre_render.R
@@ -1,6 +1,6 @@
 # Download the cruise data from the ERDDAP.
 # Existing cruise data in `./data/01_raw/` will not re-download.
-# source("R/pre_render/download_cruises.R")
+source("R/pre_render/download_cruises.R")
 
 # TODO: data preprocessing (CTD renaming?)
 
