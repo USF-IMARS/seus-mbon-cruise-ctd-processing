6b3aa370f50e5ae5ddcc2c3a9ea399e641edceb2
Tylar <murray.tylar@gmail.com>
2026-03-05 15:51:05 -0500

    working cruise reports

--- R/pre_render/pre_render.R
+++ R/pre_render/pre_render.R
@@ -2,5 +2,7 @@
 # Existing cruise data in `./data/01_raw/` will not re-download.
 source("R/pre_render/download_cruises.R")
 
+# TODO: rm -rf cruise_report/cruise_reports/
 source("R/pre_render/generate_cruise_reports.R")
+# TODO: rm -rf ctd_reports/ctd_reports/
 source("R/pre_render/generate_ctd_reports.R")
